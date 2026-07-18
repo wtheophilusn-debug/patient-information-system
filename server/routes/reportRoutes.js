0790a39ce06ed1a94d064f827312c1ce0d95e695
wtheophilusn-debug <wtheophilusn@gmail.com>
2026-07-18 23:24:20 +0200

    Fix reports route permissions for all roles

--- server/routes/reportRoutes.js
+++ server/routes/reportRoutes.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const { patientReport, consultationReport, appointmentReport, userReport } = require('../controllers/reportController');
 const { protect, authorize } = require('../middleware/authMiddleware');
 
-router.use(protect, authorize('Administrator', 'Doctor'));
+router.use(protect);
 router.get('/patients',      patientReport);
 router.get('/consultations', consultationReport);
 router.get('/appointments',  appointmentReport);
-router.get('/users',         userReport);
+router.get('/users',         authorize('Administrator', 'Doctor'), userReport);
 
 module.exports = router;
